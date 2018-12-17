98e4995da638f9563169113a1777ba449e6bd42b
Nikita Rodionov <nekonekt@gmail.com>
2018-12-17 19:16:53 +0300

    Add todos for provider package

--- pkg/auth/provider/provider.go
+++ pkg/auth/provider/provider.go
@@ -5,6 +5,16 @@ import (
 	"net/http"
 )
 
+/*
+ * todo-list:
+ * 1. OAuth providers do not need to implement Register methods. So, there should be another interface.
+ * 2. OAuth providers do not need to implement Logout. So, ...
+ * 3. Password providers do not need to implement Callback. So, ...
+ * 4. OAuth callback functions are 85% the same. So, there should be common methods.
+ * 5. OAuth callback function are the same. So, redirect should be placed upper throw the stack.
+ *            This function should return url.
+ */
+
 type IdentityProvider interface {
 	Type() string
 
