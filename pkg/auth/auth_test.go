5ce33a883b085ae2e9624f8328b72c28e21bad28
Ilia K <ki.stfu@gmail.com>
2019-04-08 19:08:49 +0300

    Fix an empty http.Request.Body in Testf  TokenBasicAuth* tests; Fix error message in TestTokenDuplicateParams

--- pkg/auth/auth_test.go
+++ pkg/auth/auth_test.go
@@ -450,6 +450,7 @@ func TestTokenInvalidMethod(t *testing.T) {
 
 func TestTokenNoBasicAuth(t *testing.T) {
 	req := newTokenRequest(t)
+	req.Body = ioutil.NopCloser(bytes.NewReader([]byte{}))
 
 	rr := serveTokenRequest(req, t) // 401 invalid_client Authorization is missing or invalid
 	if rr.Code != http.StatusUnauthorized {
@@ -466,6 +467,7 @@ func TestTokenNoBasicAuth(t *testing.T) {
 func TestTokenBasicAuthNoClientId(t *testing.T) {
 	req := newTokenRequest(t)
 	req.SetBasicAuth("", "")
+	req.Body = ioutil.NopCloser(bytes.NewReader([]byte{}))
 
 	rr := serveTokenRequest(req, t) // 401 invalid_client Authorization is missing or invalid
 	if rr.Code != http.StatusUnauthorized {
@@ -484,6 +486,7 @@ func TestTokenBasicAuthNoClientId(t *testing.T) {
 func TestTokenBasicAuthUnknownClientId(t *testing.T) {
 	req := newTokenRequest(t)
 	req.SetBasicAuth("unknown-client", "")
+	req.Body = ioutil.NopCloser(bytes.NewReader([]byte{}))
 
 	rr := serveTokenRequest(req, t) // 401 invalid_client client_id not registered
 	if rr.Code != http.StatusUnauthorized {
@@ -574,7 +577,7 @@ func TestTokenDuplicateParams(t *testing.T) {
 	if rr.Code != http.StatusBadRequest {
 		t.Errorf("unexpected status code: %v", rr.Code)
 	}
-	if rr.Body.String() != auth.ErrorContent(auth.ErrTknInvalidRequest, "duplicate parameters found")+"\n" {
+	if rr.Body.String() != auth.ErrorContent(auth.ErrTknInvalidRequest, "'code' parameter is included more than once")+"\n" {
 		t.Errorf("unexpected body: %v", rr.Body)
 	}
 }
